71d332ef6941340eecddbd2cc1f889c971c438ca
Alan Nguyen <a_wen197@yahoo.com>
2023-10-20 19:37:58 -0700

    added gpt fetcher

---
+++ gpt_fetcher.py
@@ -0,0 +1,25 @@
+import openai 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.environ.get("API_KEY")
+
+openai.api_key = SECRET_KEY
+messages = [ {"role": "system", "content":  
+              "You are a intelligent assistant."} ] 
+while True: 
+    message = input("User : ") 
+    if message: 
+        messages.append( 
+            {"role": "user", "content": message}, 
+        ) 
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", messages=messages 
+        ) 
+    reply = chat.choices[0].message.content 
+    print(f"ChatGPT: {reply}") 
+    messages.append({"role": "assistant", "content": reply}) 
